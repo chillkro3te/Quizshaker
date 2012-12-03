c72229394916e3da6423f9c3254b2cf9d0325eab
Chillkro3te <hendy2001@gmx.de>
2012-12-03 16:16:38 +0100

    added get/set Methods and basic Construktur for player-class

--- Quizshaker/src/game/player.java
+++ Quizshaker/src/game/player.java
@@ -5,7 +5,30 @@ package game;
  * @author hendrik
  *
  */
-
 public class player {
+	
+		private int id;
+		private int score;
+		
+	
+		public player(int id){
+			this.id = id;
+			this.score = 0;
+		}
 
+
+		public int getId() {
+			return id;
+		}
+
+
+		public int getScore() {
+			return score;
+		}
+
+
+		public void setScore(int score) {
+			this.score = score;
+		}
+				
 }
