6e93fd333fd1b23127ae297c8c8438a28eb52db4
Chillkro3te <hendy2001@gmx.de>
2012-12-03 13:33:22 +0100

    reworked port_handler - some work needs to be done, not functioning at the moment

--- Quizshaker/src/network/port_handler.java
+++ Quizshaker/src/network/port_handler.java
@@ -6,7 +6,25 @@ package network;
  *
  */
 
+import java.net.*;
+import java.io.*;
+
 
 public class port_handler {
 
+	static private int port = 21337; //Needs to be changed to a working port
+	static private ServerSocket ss;
+	static InputStream instream;
+
+	static private boolean port_open = false;
+	
+	public port_handler() {
+		
+	}
+	
+	public ServerSocket create(){
+		
+		return ss; 
+	}
+	
 }
