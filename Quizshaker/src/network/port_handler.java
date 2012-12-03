2bf276e6dc3c40d44f1e7193d54526f3746ae6ba
Chillkro3te <hendy2001@gmx.de>
2012-12-03 13:52:21 +0100

    port_handler reworked. Used just to open a listening port.

--- Quizshaker/src/network/port_handler.java
+++ Quizshaker/src/network/port_handler.java
@@ -6,25 +6,41 @@ package network;
  *
  */
 
+import java.io.IOException;
 import java.net.*;
-import java.io.*;
-
 
 public class port_handler {
 
 	static private int port = 21337; //Needs to be changed to a working port
 	static private ServerSocket ss;
-	static InputStream instream;
-
 	static private boolean port_open = false;
+	
+	private Socket s;
 	
 	public port_handler() {
 		
 	}
-	
-	public ServerSocket create(){
+	/**
+	 * Creates new ServerSocket. Returns open one in case of trying to open another one.
+	 * @return Socket - Ready to be read via getinputstream. 
+	 * @throws IOException
+	 */
+	public Socket create() throws IOException {
+
+		if(port_open == false) {
+			ss = new ServerSocket(port);
+			s = ss.accept();
 		
-		return ss; 
+			return s;
+		}
+		return s;
+	}
+	/**
+	 * Destroys the open Socket
+	 */
+	public void destroy() throws IOException {
+		ss.close();
+		port_open = false;
 	}
 	
 }
