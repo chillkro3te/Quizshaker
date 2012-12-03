1fb92e83f3ab8e1729615bd1426be76fb29249bd
Chillkro3te <hendy2001@gmx.de>
2012-12-03 12:50:10 +0100

    Initial Commit

---
+++ Quizshaker/src/core/core.java
@@ -0,0 +1,14 @@
+package core;
+
+public class core {
+
+	/**
+	 * core starting class
+	 */
+	public static void main() {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
